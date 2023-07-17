45936645a99781ef724362e4b2a680715f080d10
Gagan Parashar <gaganparashar127@gmail.com>
2023-07-17 18:06:38 +0530

    hi there

---
+++ html and php/logic.php
@@ -0,0 +1,65 @@
+<?php
+
+    include "config.php";
+
+    // Don't display server errors 
+    ini_set("display_errors", "off");
+
+    // Initialize a database connection
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Destroy if not possible to create a connection
+    if(!$conn){
+        die('ERROR: Could not connect. ' . mysqli_connect_error());
+    }
+
+    // Get data to display on index page
+    $sql = "SELECT * FROM data";
+    $query = mysqli_query($conn, $sql);
+
+    // Create a new post
+    if(isset($_REQUEST['new_post'])){
+        $title = $_REQUEST['title'];
+        $content = $_REQUEST['content'];
+
+        $sql = "INSERT INTO data(title, content) VALUES('$title', '$content')";
+        mysqli_query($conn, $sql);
+
+        echo $sql;
+
+        header("Location: index.php?info=added");
+        exit();
+    }
+
+    // Get post data based on id
+    if(isset($_REQUEST['id'])){
+        $id = $_REQUEST['id'];
+        $sql = "SELECT * FROM data WHERE id = $id";
+        $query = mysqli_query($conn, $sql);
+    }
+
+    // Delete a post
+    if(isset($_REQUEST['delete'])){
+        $id = $_REQUEST['id'];
+
+        $sql = "DELETE FROM data WHERE id = $id";
+        mysqli_query($conn, $sql);
+
+        header("Location: index.php");
+        exit();
+    }
+
+    // Update a post
+    if(isset($_REQUEST['update'])){
+        $id = $_REQUEST['id'];
+        $title = $_REQUEST['title'];
+        $content = $_REQUEST['content'];
+
+        $sql = "UPDATE data SET title = '$title', content = '$content' WHERE id = $id";
+        mysqli_query($conn, $sql);
+
+        header("Location: index.php");
+        exit();
+    }
+
+?>
